4fb7cf0539cedef8222a88a640e849c9fe2bf2db
yoshihbk <yokoyamayoshihiro1995@outlook.jp>
2026-04-16 14:00:49 +0900

    Fix: align name attribute formatting in rds_subnet.tf

--- terraform/rds_subnet.tf
+++ terraform/rds_subnet.tf
@@ -4,7 +4,7 @@
 # - 構成：private_subnet_1a / private_subnet_1c
 # ---------------------------------------------------------
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "rds-subnet-group"
+  name = "rds-subnet-group"
   subnet_ids = [
     aws_subnet.private_subnet_1a.id,
     aws_subnet.private_subnet_1c.id
